dc877d4c2cc0c55243cc690a9d7dc1b38aa3057f
igor5544 <igor5544@bk.ru>
2020-02-22 16:14:20 +0500

    rework apartment img load function

--- js/imgLoad.js
+++ js/imgLoad.js
@@ -3,13 +3,15 @@
 (function () {
 
   var adFormElement = document.querySelector('.ad-form');
+  var loadApartmentImgElement = adFormElement.querySelector('[name="images"]');
   var avatarChooserElement = adFormElement.querySelector('.ad-form-header__input');
   var avatarImgElement = adFormElement.querySelector('.ad-form-header__preview img');
   var apartmentChooserElement = adFormElement.querySelector('.ad-form__input');
   var apartmentImgContainerElement = adFormElement.querySelector('.ad-form__photo');
+  var apartmentPhotosContainerElement = adFormElement.querySelector('.ad-form__photo-container');
 
-  var FILE_AVATAR_TYPES = ['jpg', 'jpeg', 'png'];
-  var FILE_APARTMENT_TYPES = ['jpg', 'jpeg'];
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var MAX_PHOTOS = 16;
 
   var ApartmentImg = {
     WIDTH: 70,
@@ -17,41 +19,70 @@
   };
 
   avatarChooserElement.addEventListener('change', function () {
-    setLoadImg(avatarChooserElement, FILE_AVATAR_TYPES, setAvatarImg, avatarImgElement);
+    setAvatarImg();
   });
 
   apartmentChooserElement.addEventListener('change', function () {
-    setLoadImg(apartmentChooserElement, FILE_APARTMENT_TYPES, setApartmentImg, apartmentImgContainerElement);
+    setApartmentImgs();
   });
 
-  function setLoadImg(chooserElement, fileTypes, loadFunction, imgContainer) {
-    var file = chooserElement.files[0];
-    var fileName = file.name.toLowerCase();
+  function setAvatarImg() {
+    var file = avatarChooserElement.files[0];
 
-    var matches = fileTypes.some(function (extension) {
-      return fileName.endsWith(extension);
-    });
-
-    if (matches) {
-      var render = new FileReader();
-
-      render.addEventListener('load', function () {
-        loadFunction(imgContainer, render);
-      });
-
-      render.readAsDataURL(file);
+    if (checkFileType(file)) {
+      renderImgFile(file, loadAvatarImg, avatarImgElement);
     }
   }
 
-  function setAvatarImg(imgContainer, render) {
+  function checkFileType(file) {
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (extension) {
+      return fileName.endsWith(extension);
+    });
+
+    return matches;
+  }
+
+  function loadAvatarImg(render, imgContainer) {
     imgContainer.src = render.result;
   }
 
-  function setApartmentImg(imgContainer, render) {
-    var apartmentImgElement = apartmentImgContainerElement.querySelector('img');
+  function setApartmentImgs() {
+    var allApartmentImagesElements = adFormElement.querySelectorAll('.ad-form__photo img');
+    var maxAddImages = MAX_PHOTOS - allApartmentImagesElements.length;
+    var imagesValue = apartmentChooserElement.files.length < maxAddImages ? apartmentChooserElement.files.length : maxAddImages;
 
-    if (apartmentImgElement) {
-      apartmentImgElement.src = render.result;
+    for (var i = 0; i < imagesValue; i++) {
+      var file = apartmentChooserElement.files[i];
+
+      if (checkFileType(file)) {
+        renderImgFile(file, loadApartmentImg);
+      }
+    }
+
+    if (maxAddImages === imagesValue) {
+      loadApartmentImgElement.setAttribute('disabled', 'disabled');
+    }
+  }
+
+  function renderImgFile(file, loadFunction, imgContainer) {
+    var render = new FileReader();
+
+    render.readAsDataURL(file);
+
+    render.addEventListener('load', function () {
+      loadFunction(render, imgContainer);
+    });
+  }
+
+  function loadApartmentImg(render) {
+    var imgContainerElement = adFormElement.querySelector('.ad-form__photo');
+    var apartmentImgElement = imgContainerElement.querySelector('img');
+    var allApartmentImagesElements = adFormElement.querySelectorAll('.ad-form__photo img');
+
+    if (apartmentImgElement !== null && allApartmentImagesElements.length !== MAX_PHOTOS) {
+      buildNewImage(render.result);
     } else {
       var imgElement = document.createElement('img');
 
@@ -59,7 +90,46 @@
       imgElement.width = ApartmentImg.WIDTH;
       imgElement.height = ApartmentImg.HEIGHT;
 
-      imgContainer.appendChild(imgElement);
+      imgContainerElement.appendChild(imgElement);
+
+      imgElement.addEventListener('click', function () {
+        removeImage(imgElement, imgContainerElement);
+      });
+    }
+  }
+
+  function buildNewImage(imgURL) {
+    var fragment = document.createDocumentFragment();
+
+    fragment.appendChild(renderNewImage(imgURL));
+
+    apartmentPhotosContainerElement.appendChild(fragment);
+  }
+
+  function renderNewImage(imgURL) {
+    var newApartmentImgElement = apartmentImgContainerElement.cloneNode(true);
+    var imgElement = newApartmentImgElement.querySelector('img');
+
+    imgElement.src = imgURL;
+
+    imgElement.addEventListener('click', function () {
+      removeImage(imgElement, newApartmentImgElement);
+    });
+
+    return newApartmentImgElement;
+  }
+
+  function removeImage(img, imgContainer) {
+    var allApartmentImagesElements = adFormElement.querySelectorAll('.ad-form__photo img');
+
+    if (allApartmentImagesElements.length <= 1) {
+      img.remove();
+    } else {
+      imgContainer.remove();
+    }
+
+    if (loadApartmentImgElement.hasAttribute('disabled') === true) {
+      loadApartmentImgElement.removeAttribute('disabled');
     }
   }
 
